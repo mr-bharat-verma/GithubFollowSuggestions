cfb06ca1681864f4dadf2609033e56fac5d28b3d
Bharat Verma <bharat2491@gmail.com>
2017-11-18 17:36:10 +0530

    Hello World

---
+++ app/src/main/java/com/githubfollow/FollowSuggestionsActivity.kt
@@ -0,0 +1,12 @@
+package com.githubfollow
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class FollowSuggestionsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_follow_suggestions)
+    }
+}
