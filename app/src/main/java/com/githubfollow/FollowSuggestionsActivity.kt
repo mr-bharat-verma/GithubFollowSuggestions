0913c8348d80ceb1be063688725d67880292c1f8
Bharat Verma <bharat2491@gmail.com>
2017-11-19 14:41:33 +0530

    show initial data

--- app/src/main/java/com/githubfollow/FollowSuggestionsActivity.kt
+++ app/src/main/java/com/githubfollow/FollowSuggestionsActivity.kt
@@ -1,12 +1,63 @@
 package com.githubfollow
 
-import android.support.v7.app.AppCompatActivity
+import android.databinding.DataBindingUtil
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.text.Html
+import android.text.SpannableStringBuilder
+import android.text.method.LinkMovementMethod
+import android.widget.ImageView
+import com.bumptech.glide.Glide
+import com.githubfollow.databinding.ActivityFollowSuggestionsBinding
+import com.githubfollow.databinding.ItemGithubUserBinding
+import com.githubfollow.github.GithubClient
+import com.githubfollow.model.GithubUser
+import io.reactivex.android.schedulers.AndroidSchedulers
 
 class FollowSuggestionsActivity : AppCompatActivity() {
 
+    private val activityBinding: ActivityFollowSuggestionsBinding by lazy {
+        DataBindingUtil.setContentView<ActivityFollowSuggestionsBinding>(this,
+                R.layout.activity_follow_suggestions)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_follow_suggestions)
+        activityBinding.dataPresent = false
+        loadImage("https://cdn-images-1.medium.com/max/800/1*uHTSBqrRff8xv3UcNL82eQ.jpeg", activityBinding.placeHolder)
+    }
+
+
+    override fun onResume() {
+        super.onResume()
+        GithubClient().getUsers(100)
+                .take(3)
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe { users ->
+                    activityBinding.dataPresent = true
+                    setData(activityBinding.item1!!, users[0])
+                    setData(activityBinding.item2!!, users[1])
+                    setData(activityBinding.item3!!, users[2])
+                }
+    }
+
+    private fun setData(item1: ItemGithubUserBinding, user: GithubUser) {
+        item1.run {
+            loadImage(user.avatarUrl!!, imageUser)
+            tvUsername.text = user.userName
+            val strBuilder = SpannableStringBuilder(Html.fromHtml(getString(R.string.follow_link, user.profileUrl)))
+            tvFollow.text = strBuilder
+            tvFollow.movementMethod = LinkMovementMethod.getInstance()
+        }
+    }
+
+
+    fun loadImage(url: String, ivImg: ImageView) {
+        Glide.with(this)
+                .load(url)
+                .override(60, 60)
+                .placeholder(R.drawable.ic_launcher_foreground)
+                .error(android.R.drawable.stat_notify_error)
+                .into(ivImg)
     }
 }
--- app/src/main/java/com/githubfollow/model/GithubUser.kt
+++ app/src/main/java/com/githubfollow/model/GithubUser.kt
@@ -7,5 +7,5 @@ class GithubUser {
     @SerializedName("id") var id: String? = null
     @SerializedName("login") var userName: String? = null
     @SerializedName("avatar_url") var avatarUrl: String? = null
-    @SerializedName("url") var url: String? = null
+    @SerializedName("html_url") var profileUrl: String? = null
 }
